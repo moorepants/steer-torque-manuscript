7c6c2df5215d0f3d15d115b0cebda0df0d622c4d
Jason K. Moore <moorepants@gmail.com>
2014-09-11 17:21:31 -0400

    Updated torque wrench R file with pdf outputs.

--- src/torque-wrench.R
+++ src/torque-wrench.R
@@ -6,34 +6,37 @@
 
 rm(list = ls())
 
+"../figures" -> figDir
+"../data" -> dataDir
+
 # read the data
-data <- read.csv('torque-wrench-data.txt')
+data <- read.csv(file.path(dataDir, 'torque-wrench-data.txt'))
 
 # miles per hour to meters per second
-mph2mps = 0.44704
+mph2mps <- 0.44704
 # inch pounds to newton meters
-inchlb2nm = 0.112984829
+inchlb2nm <- 0.112984829
 
 averageSpeed <- (data$MaxSpeed + data$MinSpeed) / 2
 
-"../../figures/davisbicycle/" -> figDir
-
 # histogram of the average run speeds
-png(paste(figDir, "twrench-speed-histogram.png", sep=""),
-	width=800, height=800, pointsize=10, res=200)
+pdf(file.path(figDir, "twrench-speed-histogram.pdf"),
+    width=4, height=4, pointsize=10)
 hist(averageSpeed)
 dev.off()
 
 # histogram of the max/min torque values
-png(paste(figDir, "twrench-torque-histogram.png", sep=""),
-	width=800, height=800, pointsize=10, res=200)
-hist(abs(c(data$MinTorque, inchlb2nm * data$MaxTorque)), main ="Histogram
-of Torque Values", xlab="Absolute value of max and min torques [Nm]", breaks=25)
+pdf(file.path(figDir, "twrench-torque-histogram.pdf"),
+    width=4, height=4, pointsize=10)
+hist(abs(c(data$MinTorque, inchlb2nm * data$MaxTorque)),
+     main ="Histogram of Torque Values",
+     xlab="Absolute value of max and min torques [Nm]",
+     breaks=25)
 dev.off()
 
 # torque versus speed for all the runs
-png(paste(figDir, "twrench-torque-speed.png", sep=""),
-	width=800, height=800, pointsize=10, res=200)
+pdf(file.path(figDir, "twrench-torque-speed.pdf"),
+    width=4, height=4, pointsize=10)
 plot(0:10,-5:5, type="n", main="Max and Min Torques as a Function of Speed",
 xlab="Speed [m/s]", ylab="Torque [Nm]")
 points(mph2mps * averageSpeed, data$MinTorque, pch=19)
@@ -44,7 +47,7 @@ maneuvers <- unique(data$Maneuver)
 print(maneuvers)
 for(maneuver in levels(maneuvers)){
     print(maneuver)
-    png(paste(figDir, "twrench-", maneuver, ".png", sep=""))
+    pdf(file.path(figDir, paste("twrench-", maneuver, ".pdf", sep="")))
     x <- subset(mph2mps*averageSpeed, data$Maneuver==maneuver)
     y <- subset(data$MinTorque, data$Maneuver==maneuver)
     y2 <- subset(inchlb2nm*data$MaxTorque, data$Maneuver==maneuver)
